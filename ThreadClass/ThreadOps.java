6bd7ddf3aea73c68d299dbed8d52185c38dcce14
Brian McEntee <brian.m.mcentee@gmail.com>
2014-11-15 22:04:05 -0500

    cleaned up ThreadOps and put try-catch outside the loops that contains sleep

--- ThreadClass/ThreadOps.java
+++ ThreadClass/ThreadOps.java
@@ -1,26 +1,31 @@
+/*	This was designed to show how to conditionally put a Thread to sleep,
+	allowing another to run, when we have two Threads running concurrently. */
 
 public class ThreadOps {
 
 	public static void main(String[] args) {
 		Thread thread1 = new Thread(new CustomTask());
 		Thread thread2 = new Thread( () -> { for(int i = 0; i<50; i++)
-												System.out.println(3); });
+												System.out.println("y"); });
 
-			thread1.start();
-			thread2.start();
+		thread1.start();
+		thread2.start();
 
 	}
 }
 
 class CustomTask implements Runnable {
 	public void run() {
-		for(int i = 0; i<50; i++){
-			System.out.println(2);
-			try{
-				if(i>25) Thread.sleep(1); //Stop or yield first thread
-			}catch(InterruptedException e) {
-				System.err.println(e); 
+		//Wrap the loop in a try-catch to guarantee we end the loop on interrupt
+		try{
+
+			for(int i = 0; i<50; i++){
+				System.out.println("x");
+				if(i>25) Thread.sleep(1); //Put thread to sleep for 1 milsec	
 			}
-		}
-	}
-}
+
+		}catch(InterruptedException e) {
+			System.err.println(e); 
+		}//end try-catch
+	}//endrun
+}				
